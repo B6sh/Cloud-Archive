96f686f329a7c6a6bb4218fa84e6b9875d7c988f
TARIQ-1998 <78320858+TARIQ-1998@users.noreply.github.com>
2022-05-07 20:51:37 +0300

    Add files via upload

---
+++ frontend/src/components/profile.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { InputGroup, FormControl } from 'react-bootstrap';
+import { Button } from "@nextui-org/react";
+import Card from './card';
+import data from '../data'
+
+
+export default function profile() {
+  return (
+
+<h1> Profile information</h1>
+
+  )
+}
