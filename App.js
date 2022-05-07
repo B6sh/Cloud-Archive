3b11030bedf49a639fe8c23ea8180c117fad3c93
TARIQ-1998 <78320858+TARIQ-1998@users.noreply.github.com>
2022-05-07 20:47:49 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,34 @@
+import './App.css';
+import { BrowserRouter, Link, Route, Routes, useNavigate } from "react-router-dom";
+import Home from './components/home';
+import Book from './components/book';
+import Favorite from './components/favorite';
+import Profile from './components/profile';
+import React, { Component } from 'react'
+
+export default function App() {
+  // let history = useNavigate ();
+  // function detail(){
+  //   history.push('/books/3')
+  // }
+  return (
+      <div className="App">
+        <nav>
+          <ul className="navbar2">
+            <li><Link to='/'>Home</Link></li>
+            {/* <li><Link to='/books'>Books</Link></li> */}
+            <li><Link to='/favorite'>favorite books</Link></li>
+            <li><Link to='/profile'>Profile</Link></li>
+          </ul>
+          {/* <button >click</button> */}
+        </nav>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/favorite' element={<Favorite />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/books/:id' element={<Book />} />
+        </Routes>
+      </div>
+  )
+}
+
