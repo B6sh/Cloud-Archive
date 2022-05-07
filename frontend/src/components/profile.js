a46d78c7ce8c8d0e83e14b0b29d4941d3a92e17d
TARIQ-1998 <78320858+TARIQ-1998@users.noreply.github.com>
2022-05-08 01:24:17 +0300

    Update profile.js

--- frontend/src/components/profile.js
+++ frontend/src/components/profile.js
@@ -9,7 +9,31 @@ import data from '../data'
 export default function profile() {
   return (
 
-<h1> Profile information</h1>
 
+<div style ={{ backgroundColor: 'grey', height: "45rem"}} className='container mt-2'>
+<h1> Profile information</h1> 
+    <div className='row'>
+    <div className='col-lg-4 col-md-4 col-xl-4 col-sm-6 mt-4'>
+    <div>
+                <img style= {{ height: "30rem" }} src="https://www.clipartmax.com/png/full/171-1717870_stockvader-predicted-cron-for-may-user-profile-icon-png.png" alt="display image" />
+            </div>
+    </div>
+    <div  className='col-lg-4 col-md-4 col-xl-4 col-sm-6 mt-4'>
+        <div style={{ textAlign: 'center', marginLeft: "180px", marginTop: "100px"}}>
+    <h1 >Username:</h1>
+      <h4>Ahmed_Gh</h4>
+        
+        <h1>Name:</h1>
+      <h4>Mr. Ahmed alghamdi</h4>
+
+      <h1>Email:</h1>
+      <h4>Ahmed_Gh@gmail.com</h4>
+      </div>
+
+    </div>
+  
+</div>
+
+</div>
   )
 }
